2080fc51b2e125460eb0b22939cc23ae5cbe1189
jaywcjlove <398188662@qq.com>
2019-02-21 01:22:15 +0800

    Updates

---
+++ static/js/2e012afe.chunk.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[194],{349:function(e,n){Prism.languages.brainfuck={pointer:{pattern:/<|>/,alias:"keyword"},increment:{pattern:/\+/,alias:"inserted"},decrement:{pattern:/-/,alias:"deleted"},branching:{pattern:/\[|\]/,alias:"important"},operator:/[.,]/,comment:/\S+/}}}]);
